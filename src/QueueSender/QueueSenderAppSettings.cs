da1fb97741dcbbff7de5f5f916ccb3c02022ed37
Radu Crisan <radu.crisan@connatix.com>
2018-08-27 14:28:10 +0300

    update docs

--- src/QueueSender/QueueSenderAppSettings.cs
+++ src/QueueSender/QueueSenderAppSettings.cs
@@ -1,9 +1,21 @@
 namespace QueueSender
 {
+    /// <summary>
+    ///     The configuration class for the QueueMessageSender.  
+    /// </summary>
     public class QueueSenderAppSettings
     {
+        /// <summary>
+        /// If enabled then the QueueMessageSender records usage statistics
+        /// </summary>
         public bool StatisticsEnabled { get; set; }
+        /// <summary>
+        /// The max number of threads to be used by this component
+        /// </summary>
         public int MaxThreadCount {get;set;}
+        /// <summary>
+        /// Max number of messages to be handled by a single thread. New messages will be directed to new threads.
+        /// </summary>
         public int RequestsPerThread {get;set;}
     }
 
